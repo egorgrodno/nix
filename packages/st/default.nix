00f29e822d910fbb46d4d1acb016444a30ec12d7
Egor Zhyh <egor990095@gmail.com>
2021-12-24 17:21:22 +0100

    add more st patches, adjust st theme

--- packages/st/default.nix
+++ packages/st/default.nix
@@ -10,15 +10,20 @@
 
   environment.systemPackages =
     [ (pkgs.st.overrideAttrs (oldAttrs: rec {
-        patches =
-          [
-            ./st-nordtheme-0.8.2.diff
-            ./st-scrollback-0.8.4.diff
-            ./st-scrollback-mouse-20191024-a2c479c.diff
-            ./st-vertcenter-20180320-6ac8c8a.diff
-            ./st-anysize-0.8.1.diff
-            ./st-manual.diff
-          ];
+      patches =
+        [ ./st-scrollback-0.8.4.diff
+          ./st-scrollback-mouse-20191024-a2c479c.diff
+          ./st-vertcenter-20180320-6ac8c8a.diff
+          ./st-anysize-0.8.4.diff
+          ./st-csi_22_23-0.8.4.diff
+          ./st-osc10-20210106-4ef0cbd.diff
+          ./st-dynamic-cursor-color-0.8.4.diff
+          ./st-bold-is-not-bright-20190127-3be4cf1.diff
+          ./st-hidecursor-0.8.3.diff
+          ./st-workingdir-20200317-51e19ea.diff
+          ./st-xclearwin-20200419-6ee7143.diff
+          ./my-patch.diff
+        ];
       }))
     ];
 }
