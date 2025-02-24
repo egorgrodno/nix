045431415c5aaacf061d8a5bf9d7d769344223e2
Egor Zhyh <egor990095@gmail.com>
2025-02-24 16:10:44 +0100

    configure treesitter folding, remove nvim-ufo

--- modules/neovim/lua-config.nix
+++ modules/neovim/lua-config.nix
@@ -22,7 +22,6 @@ require'packer'.startup(function(use)
   use 'tpope/vim-eunuch'
   use 'tpope/vim-surround'
   use 'lewis6991/gitsigns.nvim'
-  use { 'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async' }
   use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
   use {
     'nvim-telescope/telescope-fzf-native.nvim',
@@ -347,48 +346,22 @@ require'nvim-treesitter.configs'.setup {
 }
 
 --------------------------------------------------------------------------------
--- UFO
+-- Folding (uses TreeSitter)
 --------------------------------------------------------------------------------
 
-local handler = function(virtText, lnum, endLnum, width, truncate)
-  local newVirtText = {}
-  local suffix = (' 󰁂 %d '):format(endLnum - lnum)
-  local sufWidth = vim.fn.strdisplaywidth(suffix)
-  local targetWidth = width - sufWidth
-  local curWidth = 0
-  for _, chunk in ipairs(virtText) do
-    local chunkText = chunk[1]
-    local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-    if targetWidth > curWidth + chunkWidth then
-      table.insert(newVirtText, chunk)
-    else
-      chunkText = truncate(chunkText, targetWidth - curWidth)
-      local hlGroup = chunk[2]
-      table.insert(newVirtText, {chunkText, hlGroup})
-      chunkWidth = vim.fn.strdisplaywidth(chunkText)
-      -- str width returned from truncate() may less than 2nd argument, need padding
-      if curWidth + chunkWidth < targetWidth then
-          suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
-      end
-      break
-    end
-    curWidth = curWidth + chunkWidth
-  end
-  table.insert(newVirtText, {suffix, 'MoreMsg'})
-  return newVirtText
-end
-vim.o.foldcolumn = '1' -- or '0'
-vim.o.foldlevel = 99 -- configurable
-vim.o.foldlevelstart = 99
-vim.o.foldenable = true
-vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldlevel = 99         -- Start with folds open but manageable
+vim.opt.foldlevelstart = 99    -- Prevents mass collapsing on first fold
 
-require('ufo').setup({
-  fold_virt_text_handler = handler,
-  provider_selector = function(bufnr, filetype, buftype)
-    return {'treesitter', 'indent'}
-  end
+vim.api.nvim_create_autocmd("BufWinLeave", {
+  pattern = "*",
+  command = "silent! mkview"
+})
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  pattern = "*",
+  command = "silent! loadview"
 })
 
 --------------------------------------------------------------------------------
