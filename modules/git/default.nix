607e6865806bf64a19fdd7ec4beaae6ac7de6b7e
Egor Zhyh <egor990095@gmail.com>
2026-07-28 13:22:52 +0200

    add tea to the git module

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -3,7 +3,7 @@
 {
   environment.systemPackages =
     if config.base.isNixosSystem
-      then with pkgs; [ git gh glab ]
+      then with pkgs; [ git gh glab tea ]
       else [];
 
   home-manager.users = forAllUsers { imports = [ ./hm.nix ]; };
